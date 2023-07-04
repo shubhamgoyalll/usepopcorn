8c17dde8d414e8ab0c6d31c08a20f9bc2143c53d
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-04 17:12:58 +0530

    Made a fully resuable component where user can enter whatever it want as a prop

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+//What if we need rating outside
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This is rated as {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
+    />
+    <StarRating size={20} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
